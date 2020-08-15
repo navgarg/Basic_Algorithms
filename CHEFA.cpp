cd9f58294eb8f69d9e80cc6e56e410c5e6eb49cb
Navya Garg <34911308+navgarg@users.noreply.github.com>
2020-08-15 10:49:21 +0530

    Create CHEFA.cpp

---
+++ CHEFA.cpp
@@ -0,0 +1,27 @@
+Link to problem: https://www.codechef.com/problems/CHEFA
+Code:
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t){
+	    int num;
+	    cin>>num;
+	    int arr[num];
+	    for(int i=0; i<num; i++){
+	        cin>>arr[i];
+	    }
+	    sort(arr, arr+num); 
+	    int sum=0;
+	    for(int i=1; i<=num; i+=2){
+	        sum += arr[num-i];
+	    }
+	    cout<<sum<<endl;
+	    t--;
+	}
+	return 0;
+}
